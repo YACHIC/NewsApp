6ff9b28644e437c886905cd36ae05ceda827b7de
Gaea <f40670@gmail.com>
2023-05-11 15:32:24 +0800

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 莊雅棋 on 2023/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
